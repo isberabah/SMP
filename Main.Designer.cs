47301a44194fc10dda7b8071370697feea0b77c3
isberabah <43341688+isberabah@users.noreply.github.com>
2020-10-12 20:49:15 +0200

    Ajoutez des fichiers projet.

---
+++ Main.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SMP
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(791, 456);
+            this.Name = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pn_topbar;
+        private System.Windows.Forms.Panel pn_newbar;
+        private System.Windows.Forms.Panel pn_count;
+      
+    }
+}
+
